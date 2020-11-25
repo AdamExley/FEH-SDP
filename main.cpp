22afdc86286241b1752c7c37eb12fcd56eed33a5
Adam Exley <34522460+AdamExley@users.noreply.github.com>
2020-11-25 09:43:29 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,12 +8,10 @@
  * @returns status code of program exit
  */
 int main() {
-    LCD.Clear();
-    LCD.WriteLine("Hello World!");
+    Chip.test();
     while (1) {
         // Run forever
     }
     return 0;
     
-   this is a new version
 }
