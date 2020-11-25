7e35b615a2c95e63db6ace35c63514019a83b766
Adam Exley <34522460+AdamExley@users.noreply.github.com>
2020-11-25 09:42:03 -0500

    Update Chip.h

--- Chip.h
+++ Chip.h
@@ -4,12 +4,18 @@ Chip header file
 Contains the class definition and prototypes for the Chip class
 */
 
+#include "FEHLCD.h"
+#include "FEHUtility.h"
+#include "FEHRandom.h"
+#include "FEHSD.h"
+
 class Chip {
   private: //private variables, etc.
     //int example_var
   
   public: //function prototypes, etc.
     //void exmple_function();
+  void test();
   
 };
 
