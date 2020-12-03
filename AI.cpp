ebb5d1f4a124983cb07ecb6f88a64f239b9f11f4
laurenpokonosky <75027550+laurenpokonosky@users.noreply.github.com>
2020-12-03 09:38:27 -0600

    Update AI.cpp

--- AI.cpp
+++ AI.cpp
@@ -4,13 +4,17 @@ Contains all code for the AI class.
 Declarations are in AI.h
 */
 
+#include "FEHRandom.h"
 #include "AI.h"
 
+int easy_move;
 
 void AI::setDifficulty(bool diff){
     difficulty = diff;
 }
 
+int rand = RandInt();
+easy_move = rand % 7;
 
 
 
