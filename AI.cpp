2887422172993bfd526378804d7d1a2e79ea857f
Adam Exley <34522460+AdamExley@users.noreply.github.com>
2020-12-04 14:21:35 -0500

    AI is getting better

--- AI.cpp
+++ AI.cpp
@@ -8,7 +8,7 @@ Declarations are in AI.h
 #include "AI.h"
 
 AI::AI()
-:difficulty(false), player_number(2)
+:difficulty(false), ai_id(2), player_id(1)
 {}
 
 void AI::setDifficulty(bool diff){
@@ -22,14 +22,52 @@ int AI::pickMove(const int game_state_array[BOARD_ROWS][BOARD_COLUMNS]){
         return easyMove();
     }
 
-    //Hard Difficulty
-    //Copy game state to local variables for easier analysis
+    //Hard Difficulty from here on
 
+    //Array to store "scores" for each possible move
+    int move_score[BOARD_COLUMNS] = {0};
+
+    //Copy game state to local variables for easier analysis
     for(int r = 0; r < BOARD_ROWS; r++){
         for(int c = 0; c < BOARD_COLUMNS; c++){
             test_game_state[r][c] = game_state_array[r][c];
             game_state[r][c] = game_state_array[r][c];
         }
+    }
+
+    //Go through each column, and thus move possible in current state
+    for(int move = 0; move < BOARD_COLUMNS; move++){
+
+        //Check to see if move is valid in master game state
+        if(!isValidMove(move, game_state)){
+            //Move is invalid
+            move_score[move] = INT_MIN; //Set to lowest possible integer value
+            continue; //Skip further analysis
+        }
+
+        //reset test_game_state to current state
+        for(int r = 0; r < BOARD_ROWS; r++){
+            for(int c = 0; c < BOARD_COLUMNS; c++){
+                test_game_state[r][c] = game_state[r][c];
+            }
+        }
+
+        //Drop chip into test_game_state at the move being analyzed
+        //Taken from Board::updateGameState()
+            //decrement r until the row r is empty
+            //In the lowest empty row, place the chip with value of ai_id
+        int r;
+        for(r = BOARD_ROWS - 1; test_game_state[r][move] != 0; r--);
+        test_game_state[r][move] = ai_id;
+
+        //Test to see if this move results in a win. If it does, return it as the move to use.
+        if(inARow(4, ai_id, test_game_state) > 0){
+            return move;
+        }
+
+
+
+
     }
 
 
@@ -44,7 +82,7 @@ int AI::easyMove(){
 }
 
 
-int AI::inARow(const int x, const int player) {
+int AI::inARow(const int x, const int player, const int array[BOARD_ROWS][BOARD_COLUMNS]) {
 	//Written by Adam Exley
 
 	//Check for X chips in a row in any orientation
@@ -73,7 +111,7 @@ int AI::inARow(const int x, const int player) {
             //If not equal, break loop
             for (int z = 0; (z < x) && equal; z++){
                 //Check values vertically
-                if (test_game_state[r + z][c] != player){
+                if (array[r + z][c] != player){
                     equal = false;
                 }
             }
@@ -102,7 +140,7 @@ int AI::inARow(const int x, const int player) {
             //If not equal, break loop
             for (int z = 0; (z < x) && equal; z++){
                 //Check values horizontally
-                if (test_game_state[r][c + z] != player){
+                if (array[r][c + z] != player){
                     equal = false;
                 }
             }
@@ -130,7 +168,7 @@ int AI::inARow(const int x, const int player) {
             //If not equal, break loop
             for (int z = 0; (z < x) && equal; z++){
                 //Check values horizontally
-                if (test_game_state[r + z][c + z] != player){
+                if (array[r + z][c + z] != player){
                     equal = false;
                 }
             }
@@ -157,7 +195,7 @@ int AI::inARow(const int x, const int player) {
             //If not equal, break loop
             for (int z = 0; (z < x) && equal; z++){
                 //Check values horizontally
-                if (test_game_state[r - z][c + z] != player){
+                if (array[r - z][c + z] != player){
                     equal = false;
                 }
             }
@@ -174,14 +212,14 @@ int AI::inARow(const int x, const int player) {
 }
 
 
-bool AI::isValidMove(int column){
+bool AI::isValidMove(int column, const int array[BOARD_ROWS][BOARD_COLUMNS]){
 	//Pietro Lavezzo
 
 	//Checks to see if a move in column is valid
-	if  (test_game_state[0][column] != 0) {      //Checking if the top row of the column is full 
+	if  (array[0][column] != 0) {      //Checking if the top row of the column is full 
 		return false;
 	}
-	else if (test_game_state[0][column] == 0) {   //Checking if the top row of the column is empty
+	else if (array[0][column] == 0) {   //Checking if the top row of the column is empty
 		return true;
 	}
 
--- AI.h
+++ AI.h
@@ -9,10 +9,12 @@
 #include "FEHRandom.h"
 #include "FEHSD.h"
 #include "config.h"
+#include <climits>
 
 
 //https://www.gamesver.com/10-helpful-tips-tricks-and-strategies-to-win-at-connect-4/
 //https://en.wikipedia.org/wiki/Connect_Four#Mathematical_solution
+//https://docs.microsoft.com/en-us/cpp/cpp/?view=msvc-160
 
 /** @brief Computer controlled AI that has easy and hard difficulties.
  *  @details Easy difficulty is a mere random number generator.
@@ -36,7 +38,12 @@ class AI {
     /** Player number of AI (default is 2).
      *  Mainly for testing by pitting AI's against each other.
      * */
-    int player_number;
+    int ai_id;
+
+    /** Player number of non-AI (default is 1).
+     *  Mainly for testing by pitting AI's against each other.
+     * */
+    int player_id;
 
     /** @author Lauren Pokonosky
      *  @brief Easy difficulty AI move generator.
@@ -46,17 +53,23 @@ class AI {
     int easyMove();
 
     /** @author Adam Exley
-     *  @brief Determines how many X-in-a-row combonations exist for a player. Uses test_game_state array.
+     *  @brief Determines how many X-in-a-row combinations exist for a player.
      *  @details Will count multiple smaller structures inside a larger structure.
      *  For example, one 4-in-a-row is also 2 instances of 3-in-a-row and 3 instances of 2-in-a-row.
+     *  Does not factor in as to if the combos are able to be played on, as this should somewhat cancel out as
+     *  the board states will be compared agaainst each other. May result in some inaccuracy.
      *  @param x The number of chips in a row to detect.
-     *  @param player The integer to analyze in the array
-     *  @returns Combonations of x-in-a-row chips found
+     *  @param player The integer to analyze in the array.
+     *  @param array Array to analyze. game_state or test_game_state
+     *  @returns Combonations of x-in-a-row chips found.
      * */
-    int inARow(const int x, const int player);
+    int inARow(const int x, const int player, const int array[BOARD_ROWS][BOARD_COLUMNS]);
   
   public: 
 
+    /** @brief Constructs AI object, setting difficulty to easy,
+     *  ai_id to 2, and player_id to 1
+     * */
     AI();
 
     /** @brief Sets difficulty variable of AI object
@@ -66,14 +79,13 @@ class AI {
     int pickMove(const int game_state_array[BOARD_ROWS][BOARD_COLUMNS]);
 
     /** @author Pietro Lavezzo
-     *  @brief Checks if a move in test_game_state is possible in a column
+     *  @brief Checks if a move in is possible in a column
      *  @details Modified from Board::isValidMove()
      *  @param column Column to see if move is valid in
-     *  @returns True/false as to if move is valid in test_game_state
+     *  @param array Which array to test validity on, test_game_state or game_state
+     *  @returns True/false as to if move is valid in given array
      * */
-    bool isValidMove(int column);
-
-  
+    bool isValidMove(int column, const int array[BOARD_ROWS][BOARD_COLUMNS]);
 
     
 };
