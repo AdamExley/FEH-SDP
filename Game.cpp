81af992d183fd8009354d907e5b1b3b2b9d32656
Adam Exley <34522460+AdamExley@users.noreply.github.com>
2020-12-05 19:11:20 -0500

    Should be ready for final commit

--- Game.cpp
+++ Game.cpp
@@ -6,37 +6,54 @@
 
 
 Game::Game()
-// Construct, setting current player to 1
+//Adam Exley
+//Construct, setting current player to 1
 :current_player(1)
 {}
 
+
+void Game::Reset(){
+    //Pietro Lavezzo
+    current_player = 1;     //Sets current player back to 1
+}
+
+
 void Game::setSingleplayer(bool single){
-    singleplayer = single;
+    //Pietro Lavezzo
+
+    singleplayer = single;      //Sets singleplayer variable
 }
 
 
 int Game::getCurrentPlayer(){
-    return current_player;
+    //Pietro Lavezzo
+
+    return current_player;      //Outputs current player variable
 }
 
 bool Game::isPlayerTurn(){
-    if(!singleplayer){
-        return true;
+    //Pietro Lavezzo 
+
+    if(singleplayer == false){
+        return true;        //In multiplayer it's always a player's turn
     }
     else
     {
         if(current_player == 1)
         {
-            return true;
+            return true;    //If singleplayer, player is 1
         }
         else
         {
-            return false;
+            return false;   //If singleplayer, AI is player 2, return false
         }
     }
 }
 
 void Game::switchPlayer(){
+    // Adam Exley
+
+    //Switch player value from 1/2
     if(current_player == 1){
         current_player = 2;
     }
@@ -44,12 +61,6 @@ void Game::switchPlayer(){
         current_player = 1;
     }
 }
-
-
-void Game::reset(){
-    current_player = 1;
-}
-
 
 
 void waitForInput(int &x, int &y){
--- Game.h
+++ Game.h
@@ -1,5 +1,4 @@
 #pragma once
-
 /** @file Game.h
  *  @brief Contains declarations of functions in use throughout the entire game and the game class
  * */
@@ -11,32 +10,58 @@
 #include "config.h"
 
 /** @brief Controls flow of the game once in play.
- *  @details Keeps track of AI/Player turns, 
+ *  @details Keeps track of AI/Player turns.
  * */
 class Game{
 
     private:
+        /** Stores the player the game is currently on
+         * */
         int current_player;
+
+        /** Stores if the game is single or multiplayer
+         *  True = singleplayer, False = Multiplayer
+         * */
         bool singleplayer;
 
 
     public:
 
+        /** @author Adam Exley
+         *  @brief Constructs, sets current player to 1
+         * */
         Game();
 
+        /** @author Pietro Lavezzo
+         *  @brief Resets current player to 1
+         * */
+        void Reset();
+
+        /** @author Pietro Lavezzo
+         *  @brief Sets singleplayer status
+         * */
         void setSingleplayer(bool single);
 
+        /** @author Pietro Lavezzo
+         *  @brief Gets current player
+         *  @returns Current player ID
+         * */
         int getCurrentPlayer();
 
-        void switchPlayer();
-
+        /** @author Pietro Lavezzo
+         *  @brief Returns if it is currently a huamn player's turn
+         *  @returns True/False as to if it is a human's turn
+         * */
         bool isPlayerTurn();
 
-        void reset();
+        /** @author Adam Exley
+         *  @brief switches the current player
+         * */
+        void switchPlayer();
 };
 
 
-
+//Non-class functions included to be used.
 
 /**
  * @brief Makes program idle until a touch is detected
