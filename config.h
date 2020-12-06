ddb5290ab58c77b80d7fe7a5838f02902139125b
Adam Exley <34522460+AdamExley@users.noreply.github.com>
2020-12-06 00:02:52 -0500

    Organized config

--- config.h
+++ config.h
@@ -3,8 +3,8 @@
  * */
 
 //Menu Options
-#define BUTTON_COLOR LCD.Scarlet    //Background color for all menu buttons
-#define TEXT_COLOR LCD.White        //Text color for all menu buttons
+#define BUTTON_COLOR SCARLET    //Background color for all menu buttons
+#define TEXT_COLOR WHITE        //Text color for all menu buttons
 
 //Board and Chip Colors
 #define BOARD_COLOR BLUE                //Color of game board
@@ -18,8 +18,7 @@
 #define HOLE_RADIUS 15  //Radius of the hole of each tile
 #define BOARD_ROWS 6    //Rows of the game board
 #define BOARD_COLUMNS 7 //Columns in the game board
-
-#define G 9.8
+#define G 9.8           //Gravity multiplier
 
 
 //Menu Options
@@ -27,20 +26,15 @@
 #define EXIT_CALL_VALUE 11      //Value other class' touch functions can return to exit game
 
 //AI Scoring Options
-#define CENTER_WEIGHT 2 //Value to weight moves towards center of board
-#define ONE_PLAYABLE 5  //Value to give a single chip that can be played to 4
-#define TWO_IN_A_ROW 15 //Value to give combonations of 2 in a row
-    //Value to give combonations of 3 in a row above base. Base value will be 2 * TWO_IN_A_ROW
-#define THREE_IN_A_ROW 45 
-    //Value to give combonations of 4 in a row above base. Base value will be 2 * THREE_IN_A_ROW + 3 * TWO_IN_A_ROW
-#define FOUR_IN_A_ROW 100000 
+#define CENTER_WEIGHT 10        //Value to weight moves towards center of board
+#define ONE_PLAYABLE 10         //Value to give a single chip that can be played to 4
+#define TWO_IN_A_ROW 30         //Value to give combos of 2 in a row that can be played to 4
+#define THREE_IN_A_ROW 85       //Value to give combos of 3 in a row that can be played to 4
+#define FOUR_IN_A_ROW 100000    //Value to give combos of 4 in a row 
 
 
 //Image Options
-
-/** Arbitrary value to set a color's RGB values to when disabled.
- * */
-#define DISABLE_VALUE 4000
+#define DISABLE_VALUE 4000 //Arbitrary value to set a color's RGB values to when disabled.
 
 /** @brief Optimization Constant.
  *  @details The number of pixels required padding a region of pixels in order to
